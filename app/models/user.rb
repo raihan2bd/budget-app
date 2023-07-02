6f8bb79b4897563117b2a430bed23c6c7d99bf95
raihan2bd <raihan2bd.official@gmail.com>
2023-07-02 15:32:15 +0600

    Add user model with it's associations and validate properties

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :categories, dependent: :destroy
+  has_many :purchases, foreign_key: 'author_id', dependent: :destroy
+
+  validates :name, :email, :password, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
