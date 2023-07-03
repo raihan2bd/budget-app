0debe2ec9c80e4305d0ae3f50c4589f1fd5ad8f3
raihan2bd <raihan2bd.official@gmail.com>
2023-07-03 17:35:43 +0600

    Add category show page to this project

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,18 @@ class CategoriesController < ApplicationController
     @unique_purchases = current_user.purchases.joins(:categories).distinct
     @total_amount = @unique_purchases.map(&:amount).sum
   end
+
+  def show
+    @category = current_user.categories.includes(:purchases).find(params[:id])
+    @purchases = @category.purchases.sort { |a, b| b.updated_at <=> a.updated_at }
+    @total_amount = @purchases.map(&:amount).sum
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    flash[:success] = "#{@category.name} category has been deleted."
+    redirect_to categories_path
+  end
 end
